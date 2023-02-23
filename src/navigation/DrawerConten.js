bcb7aa89f804a04c6beb9bf35c3c1b05fb124353
Kevin Nuñez <101608595+jumendez16@users.noreply.github.com>
2023-02-23 17:48:33 -0500

    add businesconsole screen and terminate DrawerContent

--- src/navigation/DrawerConten.js
+++ src/navigation/DrawerConten.js
@@ -1,112 +1,168 @@
 //import liraries
-import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
-import { CommonActions, DrawerActions } from '@react-navigation/native';
+import { DrawerContentScrollView, DrawerItem, DrawerItemList } from '@react-navigation/drawer';
+import { Avatar } from '@rneui/base';
 import { Icon } from '@rneui/themed';
 import React, { Component } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { Drawer } from 'react-native-paper';
+import { View, Text, StyleSheet, Switch } from 'react-native';
 import { Colors } from '../global/styles';
 // create a component
-const DrawerContent = ({navigation,props}) => {
+const DrawerContent = (props) => {
     return (
-       <DrawerContentScrollView {...props}> 
-            <Drawer.Section>
-                <DrawerItem 
-                labelStyle={{color:Colors.grey2}}
-                icon={({size}) =>(
-                    <Icon 
-                        type='material-community'
-                        name='home'
-                        color={Colors.grey2}
-                        size={size}
-                        
-                    />
-                )}
-                focused={false}
-                label='Cliente'
-                onPress={()=>{
-                    navigation.dispatch({
-                        ...(true
-                            ? DrawerActions.closeDrawer():
-                             CommonActions.navigate('RootClientTabs'))
-                    })
-                }}
-                />
-                <DrawerItem
-                 
-                icon={({focussed, size}) =>(
-                    <Icon 
-                        type='material'
-                        name='business'
-                        color={focussed ? '#7cc' :Colors.grey2}
-                        size={size}
-                    />
-                )}
-                label='Consola Restaurants'
-                />
-                <DrawerItem 
-                icon={({focussed, size}) =>(
-                    <Icon 
-                        type='material-community'
-                        name='home'
-                        color={focussed ? '#7cc' :Colors.grey2}
-                        size={size}
-                    />
-                )}
-                label='Driver Console'
-                />
-                <DrawerItem 
-                icon={({focussed, size}) =>(
-                    <Icon 
-                        type='material-community'
-                        name='home'
-                        color={focussed ? '#7cc' :Colors.grey2}
-                        size={size}
-                    />
-                )}
-                label='Promociones'
-                />
-                <DrawerItem 
-                icon={({focussed, size}) =>(
-                    <Icon 
-                        type='material-community'
-                        name='home'
-                        color={focussed ? '#7cc' :Colors.grey2}
-                        size={size}
-                    />
-                )}
-                label='Configuraciòn'
-                />
-                <DrawerItem 
-                icon={({focussed, size}) =>(
-                    <Icon 
-                        type='material-community'
-                        name='home'
-                        color={focussed ? '#7cc' :Colors.grey2}
-                        size={size}
-                    />
-                )}
-                label='Ayuda'
-                />
-            </Drawer.Section>
+        <View style={styles.container}>
            
-            <DrawerItem
-        label="exit"
-        onPress={() => Linking.openURL('https://mywebsite.com/help')}
-      />
+            <DrawerContentScrollView {...props}> 
+                <View style={{backgroundColor:Colors.buttons}}>
+                    <View style={{
+                        flexDirection:'row',
+                        alignItems:'center',
+                        paddingLeft:20,
+                        paddingVertical:10
+                        }}>
+                        <Avatar
+                            rounded
+                            avatarStyle={styles.avatar}
+                            size={75}
+                            source={{uri:'https://1.bp.blogspot.com/-rFm-eBCw_O0/Uj8LV0iPoDI/AAAAAAABCyY/pxMB6I5hQGE/s1600/minions43.jpg'}}
+                        />
+                        <View style={{marginLeft:10}}>
+                            <Text style={{
+                                fontWeight:'bold',
+                                color:Colors.pagebackground,
+                                fontSize:18
+                            }}>Will Nuñez</Text>
+                            <Text style={{
+                                color:Colors.pagebackground,
+                                fontSize:14
+                            }}>willn@gmail.com</Text>
+                        </View>
+                    </View>
+                    <View style={{flexDirection:'row',justifyContent:'space-evenly',paddingBottom:5}}> 
+                        <View style={{flexDirection:'row', marginTop:0,}}>
+                            <View style={{marginLeft:10,alignItems:'center',justifyContent:'center'}}>
+                                <Text style={{
+                                fontWeight:'bold',
+                                color:Colors.cardbackground,
+                                fontSize:18
+                                }}>1</Text>
+                                <Text style={{
+                                color:Colors.cardbackground,
+                                fontSize:14
+                                }}>Fovoritos</Text>
+                            </View>
+                        </View>
 
-       </DrawerContentScrollView>
+                        <View style={{flexDirection:'row', marginTop:10}}>
+                            <View style={{marginLeft:10,alignItems:'center',justifyContent:'center'}}>
+                            <Text style={{
+                                fontWeight:'bold',
+                                color:Colors.cardbackground,
+                                fontSize:18
+                                }}>0</Text>
+                                <Text style={{
+                                color:Colors.cardbackground,
+                                fontSize:14
+                                }}>Carrito</Text>
+                            </View>
+                        </View>
+                    </View>
+                </View>
+               
+            <DrawerItemList {...props} />
+            <DrawerItem 
+                    label='Pomociones'
+                    icon={({color,size})=>(
+                        <Icon
+                            type='material-community'
+                            name='tag-heart'
+                            color={color}
+                            size={size}
+                        />
+                    )}
+            />
+
+            <DrawerItem 
+                    label='Configuraciòn'
+                    icon={({color,size})=>(
+                        <Icon
+                            type='material-community'
+                            name='cog-outline'
+                            color={color}
+                            size={size}
+                        />
+                    )}
+            />
+
+            <DrawerItem 
+                    label='Ayuda'
+                    icon={({color,size})=>(
+                        <Icon
+                            type='material-community'
+                            name='lifebuoy'
+                            color={color}
+                            size={size}
+                        />
+                    )}
+            />
+            <View style={{borderTopWidth:1,borderTopColor:Colors.grey5}}>
+                <Text style={styles.preferences}>Preferencias</Text>
+
+                <View style={styles.switchText}>
+                        <Text style={styles.darkTemeText}>Dark theme</Text>
+                        <View style={{paddingRight:10}}>
+                            <Switch
+                                trackColor={{false: '#767577', true:'#81b0ff'}}
+                                thumbColor="#f4f3f4"
+                            />
+                        </View>
+                </View>
+            </View>
+            </DrawerContentScrollView>
+            <DrawerItem style={{marginBottom:15,borderTopColor:Colors.grey2,borderWidth:1,borderRadius:30}}
+                    label='Cerrar Sesion'
+                    icon={({color,size})=>(
+                        <Icon
+                            type='material-community'
+                            name='logout-variant'
+                            color={color}
+                            size={size}
+                        />
+                    )}
+            />
+       </View>
     );
 };
 
 // define your styles
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#2c3e50',
+        flex: 1
     },
+    avatar:{
+        borderWidth:2,
+        borderColor:Colors.pagebackground
+        
+    },
+    preferences:{
+        fontSize:16,
+        color:Colors.grey2,
+        paddingTop:20,
+        paddingLeft:20
+    },
+    switchText:{
+        flexDirection:'row',
+        alignItems:'center',
+        justifyContent:'space-between',
+        paddingLeft:20,
+        paddingVertical:5,
+        paddingRight:10
+    },
+    darkTemeText:{
+        fontSize:16,
+        color:Colors.grey2,
+        paddingTop:10,
+        paddingLeft:0,
+        fontWeight:'bold'
+    }
 });
 
 //make this component available to the app
--- src/navigation/DrawerNavigator.js
+++ src/navigation/DrawerNavigator.js
@@ -8,6 +8,7 @@ import DrawerContent from './DrawerConten';
 
 
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import BusinessConsoleScreen from '../screens/BusinessConsoleScreen';
 
 
 const BadgeIcon =withBadge(0)(Icon);
@@ -16,26 +17,53 @@ const Drawer = createDrawerNavigator();
 const DrawerNavigator = () => {
     return (
         <Drawer.Navigator drawerContent={props => <DrawerContent {...props}  />}>
-              <Drawer.Screen 
-              name='RootClientTabs'
-              component={MainTabScreen}
-              options={{
-                title:'XpressFood',
-                headerTintColor:Colors.headerText,
-                headerStyle:{
-                    backgroundColor:Colors.buttons
-                },
-                headerRight:() =>(
-                        <TouchableOpacity style={{marginRight:10}}>
-                           <BadgeIcon 
-                           type='material-community'
-                           name='cart'
-                           size={35}
-                           color={Colors.cardbackground} />
-                        </TouchableOpacity>
-                )
-              }}
-              />
+                <Drawer.Screen 
+                    name='RootClientTabs'
+                    component={MainTabScreen}
+                    options={{
+                        
+                        title:'Cliente',
+                        drawerIcon:({focussed, size}) =>(
+                            <Icon
+                                type='material-community'
+                                name='home'
+                                color={focussed ? '#7cc':Colors.grey2}
+                                size={size}
+                            />
+                        ),
+                        headerTintColor:Colors.headerText,
+                        headerStyle:{
+                            backgroundColor:Colors.buttons
+                        },
+                        headerRight:() =>(
+                                <TouchableOpacity style={{marginRight:10}}>
+                                <BadgeIcon 
+                                type='material-community'
+                                name='cart'
+                                size={35}
+                                color={Colors.cardbackground} />
+                                </TouchableOpacity>
+                        )
+                        }}
+                />
+
+                <Drawer.Screen
+                    name='BusinessScreen' 
+                    component={BusinessConsoleScreen}
+                    options={{
+                        headerShown:false,
+                        title:'Restaurants Console',
+                        drawerIcon:({focussed, size}) =>(
+                            <Icon
+                                type='material'
+                                name='business'
+                                color={focussed? '#7cc':Colors.grey2}
+                                size={size}
+                            />
+                        ),
+                        
+                        }}
+                />
         </Drawer.Navigator>
     );
 };
